97c65c4ebc79324cd083a8ccebfc2d4426e1a803
Asgedom1 <Nega@123>
2018-10-30 16:55:26 -0700

    increase or decrease by one until the position is 3 or -3

---
+++ eclipse-workspace/HelloWorld/src/Exercise8.java
@@ -0,0 +1,31 @@
+import java.io.Console;
+import java.util.*;
+public class Exercise8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("max Position"+ RandomWalk());
+	}
+	
+	private static int RandomWalk() {
+		//algorithm
+		int x = 0;
+		int maxPosition = 0;
+		Random rand = new Random();		
+		while(x > -3 && x< 3) {
+			Boolean increase = rand.nextBoolean();
+			if(increase) {
+				x++;
+			}else {
+				x--;
+			}
+			System.out.println("Position"+ x);
+			if(maxPosition < x) {
+				maxPosition = x;
+			}			
+		}
+		
+		return maxPosition;
+	}
+
+}
